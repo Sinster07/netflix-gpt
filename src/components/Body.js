ba3c53c8756df82d2ffbf67d83001eba59f67e64
Sanchit Srivastava <103961250+Sinster07@users.noreply.github.com>
2024-05-19 23:50:18 +0530

    navigation to browse page

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,25 +3,45 @@ import Br from "./Br";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 import Login from "./Login";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
-const Body = () => {    
-    const appRouter = createBrowserRouter([
-        {
-            path: "/",
-            element: <Login / >
-        },
+const Body = () => {
+  const dispatch = useDispatch();
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
 
-        {
-            path: "/browse",
-            element: <Br / >
-        }
-    ])
+    {
+      path: "/browse",
+      element: <Br />,
+    },
+  ]);
 
-    
-   
+  //useeffect is used to setup the event listener for once only
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+        // ...
+      } else {
+        dispatch(removeUser());
+        // User is signed out
+        // ...
+      }
+    });
+  }, []);
+
   return (
     <>
-     <RouterProvider router={appRouter} /> 
+      <RouterProvider router={appRouter} />
     </>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,22 @@
+// Login.js
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [signin, setSignin] = useState();
+  const [signin, setSignin] = useState(false); // Initialize with false for sign up
   const [errormessage, setErrormessage] = useState(null);
 
   const email = useRef(null);
   const password = useRef(null);
+
+  const navigate = useNavigate();
 
   const handleSubmit = () => {
     console.log(email.current.value);
@@ -31,7 +37,7 @@ const Login = () => {
           // Signed up
           const user = userCredential.user;
           console.log(user);
-          // ...
+          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -48,7 +54,7 @@ const Login = () => {
           // Signed in
           const user = userCredential.user;
           console.log(user);
-          // ...
+          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -77,11 +83,10 @@ const Login = () => {
 
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="bg-black absolute p-12 w-3/12 my-36 mx-auto right-0 left-0 text-white opacity-90 rounded-lg "
+        className="bg-black absolute p-12 w-3/12 my-36 mx-auto right-0 left-0 text-white opacity-90 rounded-lg"
       >
         <h1 className="font-bold text-3xl p-2">
-          {" "}
-          {signin ? "Sign In" : "Sign Up"}{" "}
+          {signin ? "Sign In" : "Sign Up"}
         </h1>
 
         {!signin && (
@@ -105,19 +110,17 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-800"
         />
-        <p className="text-red-700 font-bold text-lg py-2"> {errormessage}</p>
+        <p className="text-red-700 font-bold text-lg py-2">{errormessage}</p>
         <button
           className="p-4 my-6 bg-red-700 w-full rounded-lg"
           onClick={handleSubmit}
         >
-          {" "}
-          {signin ? "Sign In" : "Sign Up"}{" "}
+          {signin ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer hover" onClick={toggleSignInForm}>
-          {" "}
           {signin
-            ? " New to Netflix? SignUp Now"
-            : "Already a registered? SignIn Now"}{" "}
+            ? "New to Netflix? SignUp Now"
+            : "Already registered? SignIn Now"}
         </p>
       </form>
     </>
